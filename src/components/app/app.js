7cec942ab3ae6631e1a48ed63b59649d61f255c3
Connor Fong <connor_fong@hotmail.com>
2019-01-22 18:33:27 -0800

    Simple directory refactor, changed gulp to node-sass, 2 space tabs to 4 space tabs, removed most starter code

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './app.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Connor Fong</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
